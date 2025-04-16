ac5cf68bf4c1a6d73bdd3feb4837761f4968bbf5
Seyimarv <oluwaseyitan299@gmail.com>
2025-04-16 19:18:37 +0100

    update collections responsiveness

--- src/ui/common/components/Skeleton/collection-skeleton.tsx
+++ src/ui/common/components/Skeleton/collection-skeleton.tsx
@@ -6,7 +6,7 @@ interface CollectionsSkeleton {
 
 const CollectionsSkeleton = ({ skeletonNumber = 3 }: CollectionsSkeleton) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 gap-y-20 ">
+    <div className={`grid ${"grid-cols-1 md:grid-cols-2 lg:grid-cols-3"} gap-4 sm:gap-6 md:gap-8 gap-y-6 sm:gap-y-12 md:gap-y-20`}>
       {Array.from({ length: skeletonNumber }, (_, i) => i + 1).map((_, i) => (
         <CardSkeleton key={i} type="collections" />
       ))}
