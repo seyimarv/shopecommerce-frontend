53a68d1545b92e1bee89e37e346f2ba59a3e7809
Seyimarv <oluwaseyitan299@gmail.com>
2025-05-03 14:41:07 +0100

    update page spinner

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useListProducts } from "@/lib/data/products";
 import ProductDetails from "@/ui/product/product-details";
 import { useParams, notFound } from "next/navigation";
 import WithSkeleton from "@/ui/common/components/Skeleton/with-skeleton";
+import PageSpinner from "@/ui/common/components/spinner/page-spinner";
 
 const Product = () => {
   const params = useParams();
@@ -20,7 +21,7 @@ const Product = () => {
   }
 
   return (
-    <WithSkeleton isLoading={isLoading}>
+    <WithSkeleton isLoading={isLoading} skeleton={<PageSpinner />}>
       <ProductDetails product={data?.response?.products[0]} />
     </WithSkeleton>
   );
--- src/ui/common/components/spinner/page-spinner.tsx
+++ src/ui/common/components/spinner/page-spinner.tsx
@@ -1,8 +1,8 @@
 import { AiOutlineLoading3Quarters } from "react-icons/ai"
 
-const PageSpinner = ({ className, size }: { className?: string, size?: number }) => {
+const PageSpinner = ({ className, size = 30 }: { className?: string, size?: number }) => {
     return (
-        <div className="flex items-center justify-center min-h-[90vh]">
+        <div className="flex items-center justify-center min-h-[60vh]">
             <AiOutlineLoading3Quarters className={`animate-spin ${className}`} size={size} />
         </div>
     )
